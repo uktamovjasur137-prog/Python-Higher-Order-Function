dd0c2b6b55ed427f3af69f0dfd1651413815dc2c
Jasur <uktamovjasur137@gmail.com>
2026-04-17 02:05:05 -0700

    done

--- task16.py
+++ task16.py
@@ -1,4 +1,4 @@
 data = [123, "apple", "banana", "cat", 456, "mango", "elephant"]
 
-result = filter(lambda x: len(x) <= 5, filter(lambda x: isinstance(x, str), data))
+result = filter(lambda x: len(x) <= 5, filter(lambda x: type(x) == str, data))
 print(list(result))
